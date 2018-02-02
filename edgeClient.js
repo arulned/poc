98d6cc6f0cb8c7f31cb595a73e043f64b87b27f8
Arul <anedumar@akamai.com>
2018-02-02 11:48:06 -0500

    added test

---
+++ edgeClient.js
@@ -0,0 +1,45 @@
+'use strict';
+let untildify = require("untildify");
+var EdgeGrid = require("edgeGrid");
+const winston = require("winston");
+
+var logger = new winston.Logger({
+  level: process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "debug",
+  transports: [
+    new winston.transports.Console()
+  ]
+});
+
+class Edge {
+    constructor(auth = {
+        path: "~/.edgerc",
+        section: "papi",
+        debug: false,
+        default: true
+      }) {
+        if (auth.clientToken && auth.clientSecret && auth.accessToken && auth.host) {
+          this._edge = new EdgeGrid(auth.clientToken, auth.clientSecret, auth.accessToken, auth.host, auth.debug);
+        } else {
+          this._edge = new EdgeGrid({
+            path: untildify(auth.path),
+            section: auth.section,
+            debug: auth.debug
+          });
+        }
+      }
+
+      get(request) {
+        return new Promise((resolve,reject)=>{
+          this._edge.auth(request);
+          this._edge.send(function (data, response) {
+            if (response && response.statusCode >= 200 && response.statusCode < 400) {
+              resolve(JSON.parse(response.body));
+            } else { 
+              reject("Could not get configurations at this time.");
+            }
+          });
+        });
+      }
+}
+
+module.exports = Edge;
--- test/test.js
+++ test/test.js
@@ -1,11 +1,32 @@
-var ApiService = require("../service");
-var expect = require('chai').expect;
+/* global describe it before*/
+//var expect = require("chai").expect;
+//var sinon = require("sinon");
+var AppSecConfig = require("../appSecConfig");
+var nock = require("nock");
 
-var apiService = new ApiService();
+describe("AppSecConfig.configs()", function () {
+  var twitstat = new AppSecConfig();
+  var scope;
+  before(function () {
+    scope = nock(/.*.akamaiapis.net/)
+                .get('/v1/configs')
+                .reply(200, { hello: 'worldeeee' });
+  });
 
-describe("ApiService.add()", function(){
-  it("should add two numbers",function(){
-    let result = apiService.add(5,6);
-    expect(result).to.equal(11);
+  it(" should return all configuration", function () {
+
+    //var expectedEndpoint = "/v1/configs";
+    // var body = JSON.stringify({
+    //   count: 9,
+    //   url: "http://some-url.com/"
+    // });
+    
+    //edgegrid.onFirstCall().returns(body);
+    twitstat.configs(function(resp){
+      console.log('==='+JSON.stringify(resp));
+    });
+
+    console.log(scope.isDone());
+    //expect(result).to.equal(11);
   });
 });
